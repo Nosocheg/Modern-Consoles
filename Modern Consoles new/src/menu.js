ca99a6c8da9870719d82aebdb86b2c0d36d77a4b
Nosocheg <95674618+Nosocheg@users.noreply.github.com>
2021-12-20 10:58:57 +0300

    Add files via upload

---
+++ Modern Consoles new/src/menu.js
@@ -0,0 +1,5 @@
+/* Переключение между добавлением и удалением класса "responsive" в topnav, когда пользователь нажимает на значок */
+
+function myFunction() {
+  $('[id="myTopnav"]').toggleClass('responsive')
+}
